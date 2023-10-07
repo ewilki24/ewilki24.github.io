c0dce0167334192f191483f48d9dd6f57cd6d154
ewilki24 <emilywilk0388@gmail.com>
2023-10-07 19:34:52 -0400

    commit

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,5 +2,60 @@ function scriptTest() {
     alert("hey my script is running")
 }
 
+function submitName() {
+    document.getElementById("name").submit();
+}
 
+function submitMood() {
+    document.getElementById("good").submit();
+    document.getElementById("okay").submit();
+    document.getElementById("bad").submit();
+}
 
+function findPolygon() {
+    let x = " "
+
+    if (x === 1) {
+        alert("That's a Henagon");
+    } else if (x === 2) {
+        alert("That's a Digon");
+    } else if (x === 3) {
+        alert("That's a Trigon");
+    } else if (x === 4) {
+        alert("That's a Tetragon");
+    } else if (x === 5) {
+        alert("That's a Pentagon");
+    } else if (x === 6) {
+        alert("That's a Hexagon");
+    } else if (x === 7) {
+        alert("That's a Heptagon");
+    } else if (x === 8) {
+        alert("That's a Octagon");
+    } else if (x === 9) {
+        alert("That's a Enneagon");
+    } else if (x === 10) {
+        alert("That's a Decagon");
+    } else {
+        alert("New Number Please :)")
+    }
+}
+
+function receiveComplement() {
+    alert("You have a great smile :)");
+}
+
+function getMotivation() {
+    alert("You won't get were you want to be without taking risk and trying, so just try it.");
+}
+
+function addSum() {
+    alert("It's 19... Don't think I'll say 21 :D");
+}
+
+function wolfFact() {
+    alert("Wolves live up to 13 years in the wild, typically around 6 to 8. However, up to 16 years in captivity.");
+}
+
+function goodCandy() {
+    alert("I recommend mini M&M's or Hairbo Gummy Bears!");
+}
