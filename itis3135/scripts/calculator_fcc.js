a4dfcbdf5e969aa072531c8439ff7932caf2491f
ewilki24 <emilywilk0388@gmail.com>
2023-10-22 16:13:11 -0400

    update

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -6,26 +6,24 @@ const action = key.dataset.action;
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
-        //do something
+        if (!action) {
+            console.log('number key!');
+        }
+        
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            console.log("operator key!");
+        }
+        
+        if (action === 'decimal') {
+            console.log("decimal key!");
+        }
+        
+        if (action === 'clear') {
+            console.log("clear key!");
+        }
+        
+        if (action === 'calculate') {
+            console.log("equal key!");
+        }
     }
 })
-
-if (!action) {
-    console.log('number key!');
-}
-
-if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-    console.log('operator key!');
-}
-
-if (action === 'decimal') {
-    console.log('decimal key!');
-}
-
-if (action === 'clear') {
-    console.log('clear key!');
-}
-
-if (action === 'calculate') {
-    console.log('equal key!');
-}
