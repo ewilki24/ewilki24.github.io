e4849f1d948dec8cfa9c6247a0c8fb91b6c39eb5
ewilki24 <emilywilk0388@gmail.com>
2023-10-22 16:19:24 -0400

    commit

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -7,23 +7,38 @@ const action = key.dataset.action;
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         if (!action) {
-            console.log('number key!');
-        }
-        
-        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            console.log("number key!");
+        } else if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
             console.log("operator key!");
-        }
-        
-        if (action === 'decimal') {
+        } else if (action === 'decimal') {
             console.log("decimal key!");
-        }
-        
-        if (action === 'clear') {
+        } else if (action === 'clear') {
             console.log("clear key!");
-        }
-        
-        if (action === 'calculate') {
+        } else if (action === 'calculate') {
             console.log("equal key!");
         }
     }
 })
+
+/*
+if (!action) {
+    console.log('number key!');
+}
+
+if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+    console.log("operator key!");
+}
+
+if (action === 'decimal') {
+    console.log("decimal key!");
+}
+
+if (action === 'clear') {
+    console.log("clear key!");
+}
+
+if (action === 'calculate') {
+    console.log("equal key!");
+}
+*/
+
