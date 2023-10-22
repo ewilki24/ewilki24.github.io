4c4aa83f2c66e9f52d5da67f44f7b2b98c961721
ewilki24 <emilywilk0388@gmail.com>
2023-10-22 14:13:36 -0400

    add

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,13 @@
+function submit() {
+    document.getElementById("num");
+}
+
+function calculator() {
+    if (1 && 3) {
+        alert("4");
+    
+        /* I know that this is not much but I'm
+        having a hard time figuring out is this is how
+        is it done or an easier way to do this. */
+    }
+}
