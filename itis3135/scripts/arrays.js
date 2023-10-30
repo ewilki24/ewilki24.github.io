1ebb5eeefbe7f5ccc7d3d88ac084b19def6ee545
ewilki24 <emilywilk0388@gmail.com>
2023-10-29 20:10:47 -0400

    commit

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,23 @@
+let name = [];
+let salary = [];
+
+function displayResults() {
+    let s = document.getElementById(results);
+    let result = s/7;
+    alert(result);
+}
+
+function displaySalary() {
+    let ns = document.getElementById(results_table);
+    return ns;
+}
+
+function addSalary() {
+    for (i = 0; i < name.length(); i++) {
+        add.getElementById(new_name);
+    }
+
+    for(i = 0; i < salary.length(); i++) {
+        add.getElementById(new_salary);
+    }
+}
