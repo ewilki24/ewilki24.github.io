860e73d2ed7f324c58a6ed3c73b1f4fc6c2065cc
ewilki24 <emilywilk0388@gmail.com>
2023-11-10 15:12:50 -0500

    update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,40 +1,5 @@
-
-function submitName() {
-    document.getElementById("name").submit();
-}
-
-function submitMood() {
-    document.getElementById("good").submit();
-    document.getElementById("okay").submit();
-    document.getElementById("bad").submit();
-}
-
 function findPolygon() {
-    let x = " "
-
-    if (x === 1) {
-        alert("That's a Henagon");
-    } else if (x === 2) {
-        alert("That's a Digon");
-    } else if (x === 3) {
-        alert("That's a Trigon");
-    } else if (x === 4) {
-        alert("That's a Tetragon");
-    } else if (x === 5) {
-        alert("That's a Pentagon");
-    } else if (x === 6) {
-        alert("That's a Hexagon");
-    } else if (x === 7) {
-        alert("That's a Heptagon");
-    } else if (x === 8) {
-        alert("That's a Octagon");
-    } else if (x === 9) {
-        alert("That's a Enneagon");
-    } else if (x === 10) {
-        alert("That's a Decagon");
-    } else {
-        alert("New Number Please :)")
-    }
+    
 }
 
 function receiveComplement() {
