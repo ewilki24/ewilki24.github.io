2ee3055fefa494257e19e97f7c10d543be0b3ed5
ewilki24 <emilywilk0388@gmail.com>
2023-11-14 16:41:26 -0500

    commit

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(800, 700);
+    noLoop();
+}
+  
+function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 40, 40);
+}
+
+function mousePressed() {
+    loop();
+}
+
+function mouseReleased() {
+    noLoop();
+}
