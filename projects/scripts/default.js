939e9a78ea211bbc75af45b2f5b98e303bd557a0
ewilki24 <emilywilk0388@gmail.com>
2023-12-09 12:55:53 -0500

    commit

--- projects/scripts/default.js
+++ projects/scripts/default.js
@@ -1,5 +1,5 @@
 function fakeComment() {
-    if (document.getElementById(comment).onsubmit) {
+    if (document.getElementById(comment).onclick) {
         alert("Sorry, this purposely doesn't work. Thank you for the comment!");
     } 
 }
